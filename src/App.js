92f670532ddc3ddcaea94b536970ac989f44a355
Victor Barreto <denvictor.sbarreto@gmail.com>
2022-09-17 12:00:05 -0300

    feat: Adicionando carousel

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import "./App.css";
 import Navbar from "../src/components/layout/Navbar/Navbar";
+import Slider from "../src/components/layout/Slider/Slider";
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Slider />
     </Router>
   );
 }

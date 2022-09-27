e7ade7dfbdfdf49d77977b79fa675a60a6f017ce
KelvenGeovane <geovanekelven.contato@gmail.com>
2022-09-27 16:05:21 -0300

    Adicionado Tela Login

---
+++ src/components/pages/Login/Login.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "../Login/Login.css";
+
+import Navbar from "../../layout/Navbar/Navbar";
+
+function Login() {
+  return (
+    <div>
+      <Navbar />
+      <div className="login">
+      <h1>Login</h1>
+        <p className="padLogin">
+        <input className="loginInput" type="text" placeholder="Nome"></input>
+        </p>
+        <br></br>
+        <input className="loginInput" type="password" placeholder="Senha"></input>
+        <br></br>
+        <p className="padLogin">
+        <button className="loginEnviar">Enviar</button>
+        </p>
+        </div>
+    </div>
+  );
+}
+
+export default Login;

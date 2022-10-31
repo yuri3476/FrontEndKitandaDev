5d735e71997be335b9971d4c9f37000ecbc12cdb
Victor Barreto <denvictor.sbarreto@gmail.com>
2022-10-31 13:42:49 -0300

    Adiciona animação na tela de login

--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -7,21 +7,27 @@ function Login() {
   return (
     <div>
       <Navbar />
-      <div className="login">
-        <h1>Login</h1>
-        <p className="padLogin">
-          <input className="loginInput" type="text" placeholder="Nome"></input>
-        </p>
-        <br></br>
-        <input
-          className="loginInput"
-          type="password"
-          placeholder="Senha"
-        ></input>
-        <br></br>
-        <p className="padLogin">
-          <button className="loginEnviar">Enviar</button>
-        </p>
+      <div className="container-animation">
+        <div class="login">
+          <h1>Login</h1>
+          <p className="padLogin">
+            <input
+              className="loginInput"
+              type="text"
+              placeholder="Nome"
+            ></input>
+          </p>
+          <br></br>
+          <input
+            className="loginInput"
+            type="password"
+            placeholder="Senha"
+          ></input>
+          <br></br>
+          <p className="padLogin">
+            <button className="loginEnviar">Enviar</button>
+          </p>
+        </div>
       </div>
     </div>
   );

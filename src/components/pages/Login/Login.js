e5e279261df68416f8099b91bf74dabdac1a06c5
KelvenGeovane <geovanekelven.contato@gmail.com>
2022-11-12 11:18:17 -0300

    Revert "LoginValidation"
    This reverts commit 3ed33ad82d5a0a51bda2c8f7539509288345d301.

--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -1,28 +1,11 @@
-import React, {useState} from 'react';
+import React from "react";
 import "../Login/Login.css";
 
-function inicialState(){
-  return {user: '', password: ''};
-}
 
-
-const Login = () => {
-
-  const [values,setValues] = useState();
-
-  function onChange(event){
-    const {value, name} = event.target;
-
-    setValues({
-      ...values,
-      [name]:value,
-    })
-  }
-
+function Login() {
   return (
     <div>
       <div className="container-animation">
-        <form>
         <div class="login">
           <h1>Login</h1>
           <p className="padLogin">
@@ -30,9 +13,6 @@ const Login = () => {
               className="loginInput"
               type="text"
               placeholder="Nome"
-              name="user"
-              onChange={onChange}
-              value={values.user}
             ></input>
           </p>
           <br></br>
@@ -40,16 +20,12 @@ const Login = () => {
             className="loginInput"
             type="password"
             placeholder="Senha"
-            name="password"
-            onChange={onChange}
-            value={values.password}
           ></input>
           <br></br>
           <p className="padLogin">
             <button className="loginEnviar">Enviar</button>
           </p>
         </div>
-        </form>
       </div>
     </div>
   );
